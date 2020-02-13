1cea22825dba1fdd475d129a2be8ba2c14afc4e2
Andrea Bottini <andrea.bottini22@gmail.com>
2020-02-13 15:55:45 +0100

    Impostazione primo blocco lavoro e prima chiamata Ajax

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
 // alert('funziona?')
+// Todoolean
+// Creiamo una app che permette di inserire e cancellare dei todos in una lista utilizzando la API boolean per fare operazioni CRUD.
+// BONUS
+// Aggiungiamo update del todo
+// http://157.230.17.132:3003/todos
+
+$(document).ready(function() {
+
+
+  $.ajax({
+    url: 'http://157.230.17.132:3003/todos',
+    method: 'GET',
+    success: function(data){
+      console.log(data);
+    },
+    error: function(request, state, errors) {
+      alert('è avvenuto un errore')
+    }
+
+  });
+});
